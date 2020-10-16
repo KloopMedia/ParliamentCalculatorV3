0e1c2cbd6ca81edae928f82c31e82376e536924d
xakim <xakim@pop-os.localdomain>
2020-10-16 13:06:56 +0600

    Updates

---
+++ precache-manifest.d914e193beae54ecd286e6ad484254a7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075dc5a8b6b060d5bcd42e0d5f2edad1",
+    "url": "/ParliamentCalculatorV3/index.html"
+  },
+  {
+    "revision": "1f560dff47dbdd03821c",
+    "url": "/ParliamentCalculatorV3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "732a9d037f32be9c6198",
+    "url": "/ParliamentCalculatorV3/static/js/2.4741c04d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/ParliamentCalculatorV3/static/js/2.4741c04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f560dff47dbdd03821c",
+    "url": "/ParliamentCalculatorV3/static/js/main.3313ab87.chunk.js"
+  },
+  {
+    "revision": "5371821d7a7d75a81750",
+    "url": "/ParliamentCalculatorV3/static/js/runtime-main.4d00acbf.js"
+  },
+  {
+    "revision": "a2cb1bc2027d2433b31690676cadac1d",
+    "url": "/ParliamentCalculatorV3/static/media/PARTIES_RESULTS_REACT.a2cb1bc2.csv"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ParliamentCalculatorV3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6ae188912c1b4c4c29c4de7342d12f1",
+    "url": "/ParliamentCalculatorV3/static/media/Бир Бол.d6ae1889.png"
+  },
+  {
+    "revision": "5f98b378fce0a6bd8d166083d5432ee3",
+    "url": "/ParliamentCalculatorV3/static/media/Кыргызстан.5f98b378.png"
+  },
+  {
+    "revision": "b9d0c5d3dc30b0d0bce829e7b98c7807",
+    "url": "/ParliamentCalculatorV3/static/media/Мекен Ынтымагы.b9d0c5d3.png"
+  },
+  {
+    "revision": "d975dd8f04f9be16f265c3983a306475",
+    "url": "/ParliamentCalculatorV3/static/media/Политическая партия ветеранов войны в Афганистане и участников других боевых конфликтов.d975dd8f.png"
+  },
+  {
+    "revision": "3f7000b1ad13ad7b4a33f2cefb24a9f0",
+    "url": "/ParliamentCalculatorV3/static/media/Чон казат.3f7000b1.png"
+  },
+  {
+    "revision": "e1489fd6f4537782821aecc61003e5a5",
+    "url": "/ParliamentCalculatorV3/static/media/Ыйман Нуру.e1489fd6.png"
+  }
+]);
